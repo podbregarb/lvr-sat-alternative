5b36ad10784245048bf908fa2769620c2338e9c0
Andrej Bauer <Andrej.Bauer@andrej.com>
2014-03-28 11:35:45 +0100

    Podatkovna struktura za CNF obliko.

--- primer.py
+++ primer.py
@@ -2,8 +2,17 @@
 # -*- encoding: utf-8 -*-
 
 from bool import *
+from cnf import *
 
 # Formula: x /\ not (not y \/ x) /\ z
 formula1 = And([Atom("x"), Not(Or([Not(Atom("y")), Atom("x")])), Atom("z")])
 
+# NNF oblika
 formula2 = formula1.nnf()
+
+# Primer CNF formule:
+cnf1 = Cnf([
+         Stavek([]),
+         Stavek([Lit("x"), Lit("y"), Til("z")]),
+         Stavek([Lit("z")]),
+       ])
