77a0e742273a7502a912ee8abf3f0f8aae319248
podbregarb <barbara.podbregar@gmail.com>
2014-04-23 19:50:20 +0200

    Popravili dpll.
    Trenutno preverja stavke dolžine 0 (vrne ni rešitve) in 1 (nastavi
    ustrezne spremenljivke in poenostavi formulo).

--- dpll.py
+++ dpll.py
@@ -5,17 +5,16 @@ def dpll(formula):
     formula=formula.cnf()
     znane_spr={}
     neznane_spr=[]
+    spr=[]
     for j in range(0,len(formula.stavki)):
         for k in range(0,len(formula.stavki[j].literali)):
-            if str(formula.stavki[j].literali[k].ime) not in neznane_spr:
-                neznane_spr = neznane_spr + [str(formula.stavki[j].literali[k].ime)]
-    print (neznane_spr)
+            neznane_spr = neznane_spr + [str(formula.stavki[j].literali[k].ime)]
+            if str(formula.stavki[j].literali[k].ime) not in spr:
+                spr = spr + [str(formula.stavki[j].literali[k].ime)]
     # CS je seznam še neobdelanih stavkov
     CS1=formula.stavki
     CS=list(CS1)
-    print(CS1)
     for i in CS1:
-        print(CS)
         if i.literali==[]:
             print ("Ni rešitve.")
             break
@@ -25,9 +24,9 @@ def dpll(formula):
                 neznane_spr.remove(i.literali[0].ime)
                 for k in CS1:
                     for j in k.literali:
-                        if j.ime==i.literali[0].ime and isinstance(j,Lit):
+                        if j.ime==i.literali[0].ime and isinstance(j,Lit) and k in CS:
                             CS.remove(k)
-                        if isinstance(j,Til) and j.ime==i.literali[0].ime:
+                        if isinstance(j,Til) and j.ime==i.literali[0].ime and k in CS:
                             r=CS.index(k)
                             CS[r].literali.remove(j)
             else:
@@ -35,19 +34,16 @@ def dpll(formula):
                 neznane_spr.remove(i.literali[0].ime)
                 for k in CS1:
                     for j in k.literali:
-                        if isinstance(j,Til) and j.ime==i.literali[0].ime:
+                        if isinstance(j,Til) and j.ime==i.literali[0].ime and k in CS:
                             CS.remove(k)
-                        if j.ime==i.literali[0].ime and isinstance(j,Lit):
+                        if j.ime==i.literali[0].ime and isinstance(j,Lit) and k in CS:
                             r=CS.index(k)
                             CS[r].literali.remove(j)
-            
-
-    print(neznane_spr)
-    print(CS)
-    return znane_spr
+    print(znane_spr)
+    return CS
     
 f=And([Or([Atom('a')]),Or([Atom('b'),Atom('c'),Atom('a')]),Or([Atom('c'),Atom('d'),Not(Atom('a'))]),Or([Atom('b'),Atom('c')]),Or([Atom('a')])])
 g=And([Or([Not(Atom('a'))]),Or([Atom('b'),Atom('c'),Atom('a')]),Or([Atom('c'),Atom('d'),Not(Atom('a'))]),Or([Atom('b'),Atom('c')]),Or([Not(Atom('a'))])])
-            
-            
+h=And([Or([Atom('a')]),Or([Atom('b'),Atom('c'),Atom('d')]),Or([Atom('c'),Atom('a'),Atom('b')]),Or([Not(Atom('b'))])])
+test=And([Or([Atom('a')]),Or([Not(Atom('a'))])])           
             
