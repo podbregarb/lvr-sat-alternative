eb0539c1882358fb139eabe8555052518ef4a17a
podbregarb <barbara.podbregar@gmail.com>
2014-04-30 14:21:16 +0200

    Zbrisani programi, ki niso vodili nikamor :P

--- povezanost.py
+++
@@ -1,8 +0,0 @@
-from bool import *
-from cnf import *
-
-def povezanost(V,E):
-    #V je stevilo vozlisc
-    #E je seznam parov (vozlisca, ki so med sabo povezana), vozlisce z manjso cifro je na prvi komponenti
-    
-    
