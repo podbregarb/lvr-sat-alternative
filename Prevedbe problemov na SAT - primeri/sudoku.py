c8aad5d646e6f20c779063612e8b215b60d95cf1
podbregarb <barbara.podbregar@gmail.com>
2014-05-06 19:23:03 +0200

    Lukov sudoku.

--- Prevedbe problemov na SAT - primeri/sudoku.py
+++ Prevedbe problemov na SAT - primeri/sudoku.py
@@ -1,17 +1,14 @@
-# -*- encoding: utf-8 -*-
-
 from bool import *
 from cnf import *
 
 def sudoku(seznam):
-    # seznam je seznam (i,j,k) - na i,j - tem mestu je stevilka k
+    # seznam je seznam trojk (i,j,k) - na i,j - tem mestu je cifra k
 
 
-    # banana predstavlja seznam vnaprej poznanih cifer/spremenljivk
-    banana=[]
+    # znane predstavlja seznam znanih cifer
+    znane=[]
     for i in seznam:
-        banana.append(Atom((i)))
-
+        znane.append(Atom((i)))
 
 
     # vsako polje ima vsaj eno cifro
@@ -24,7 +21,7 @@ def sudoku(seznam):
             r.append(Or(r2))
 
             
-    # nima hkrati dveh cifer na istem mestu
+    # nobeno polje nima več kot eno cifro
     s=[]
     for i in range(1,10):
         for j in range (1,10):
@@ -33,28 +30,28 @@ def sudoku(seznam):
                     s.append(Not(And([Atom((i,j,k)),Atom((i,j,l))])))
 
                     
-    # v vrstici/stolpcu ni dveh istih cifer
+    # v nobeni vrstici/stolpcu ni istih cifer
     t=[]
     for i in range(1,10):
         for j in range(1,10):
-            for l in range(1,10):
-                for k in range(1,10):
-                    t1=Not(Or([And([Atom((i,j,k)),Atom((i,l,k))]),And([Atom((i,j,k)),Atom((l,j,k))])]))
+            for k in range(1,j):
+                for l in range(1,10):
+                    t1=Not(Or([And([Atom((i,j,l)),Atom((i,k,l))]),And([Atom((j,i,l)),Atom((k,i,l))])]))
         t.append(t1)
 
 
-    # v vsakem 3x3 kvadratku :) ni dveh dveh istih cifer
-    luka=[]
-    for alenka in range(0,3):
-        for i in range(1,4):
-            for j in range(1,4):
-                for l in range(1,4):
-                    for barbka in range(1,10):
-                        luka1=Not(Or([And([Atom((i+alenka*3,j+alenka*3,barbka)),Atom((i+alenka*3,l+alenka*3,barbka))]),And([Atom((i+alenka*3,j+alenka*3,barbka)),Atom((l+alenka*3,j+alenka*3,barbka))])]))
-        luka.append(luka1)
+    # v nobenem 3x3 kvadratku ni istih cifer
+    v=[]
+    for x in range(0,3):
+        for y in range(0,3):
+            for i in range(1,4):
+                for j in range(1,4):            
+                    for l in range(1,10):
+                        w=Not(Or([And([Atom((i+x*3,j+y*3,l)),Atom((i+x*3,l+y*3,l))])]))
+                    v.append(w)
                                   
 
-    return And(r+s+t+luka+banana)
+    return And(r+s+t+v+znane)
 
 
 primer=[(1,1,5),(1,2,3),(1,5,7),(2,1,6),(2,4,1),(2,5,9),(2,6,5),(3,2,9),(3,3,8),(3,8,6),(4,1,8),(4,5,6),(4,9,3),(5,1,4),(5,4,8),(5,6,3),(5,9,1),(6,1,7),(6,5,2),(6,9,6),(7,2,6),(7,7,2),(7,8,8),(8,4,4),(8,5,1),(8,6,9),(8,9,5),(9,5,8),(9,8,7),(9,9,9)]
