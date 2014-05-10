657afb4eecf9f2c9df91849bf8bdec7de963a133
podbregarb <barbara.podbregar@gmail.com>
2014-05-10 23:11:02 +0200

    Dela tudi če sam vpišeš sudoku.
    Treba je preveriti še če ni rešitve, oz če je reštev več. I'm on it.

--- demo.py
+++ demo.py
@@ -21,25 +21,6 @@ def test_dpll():
 
 # uporaba dpll na sudokuju in barvanju grafov
 def sudoku4x4():
-    print ('Lahko vpises svoj 4x4x sudoku v obliki [[1. vrstica],[2. vrstica],...]. Polja, ki so prazna naj vsebujejo stevilo 0. Ce ne zelis vpisati svojega sudokuja, bo program resil nekaj vnaprej dolocenih primerov, ki so v Prevedbe_problemov/sudoku4.txt.')
-    odlocitev=input('Ali zelis vpisati svoj sudoku? (y/n)  ')
-    if odlocitev=='n':
-        f=open('Prevedbe_problemov/sudoku4.txt','r')
-        f=f.readlines()
-        seznam=[]
-        for i in range(len(f)):
-            for j in range(0,7,2):
-                if f[i][j]!='0':
-                    seznam.append((i+1,j//2+1,int(f[i][j])))
-## to se delam: :D
-##    else:
-##        sud=input('Vpisi svoj sudoku:  \n')
-##        sud=sud[1:-1].split('[')
-##        print(sud)
-    resitev=dpll.dpll(sudoku.sudoku4(seznam))[1]
-    for i in resitev.copy():
-        if resitev[i]==False:
-            del resitev[i]
     def risanje(resitev):
         window=Tk()
         canvas=Canvas(window, width=200, height=200)
@@ -52,9 +33,34 @@ def sudoku4x4():
         canvas.create_line(15,60,185,60)
         canvas.create_line(15,100,185,100, width=3)
         for i in resitev.keys():
-            canvas.create_text(i[1]*40,i[0]*40,text='{0}'.format(i[2]),font=('Arial',25))
-            
+            canvas.create_text(i[1]*40,i[0]*40,text='{0}'.format(i[2]),font=('Arial',25))  
         window.mainloop()
+    print('Ta program resuje 4x4 sudoku.')
+    odlocitev=input('Ali zelis vpisati svoj sudoku? (y/n)  ')
+    if odlocitev=='n':
+        f=open('Prevedbe_problemov/sudoku4.txt','r')
+        f=f.readlines()
+        seznam=[]
+        for i in range(len(f)):
+            for j in range(0,7,2):
+                if f[i][j]!='0':
+                    seznam.append((i+1,j//2+1,int(f[i][j])))
+## to se delam: :D
+    else:
+        print ('Sudoku naj bo oblike "1 2 0 0,0 3 4 0,...", kjer 0 pomeni, da je polje prazno.')
+        sud=input('Vpisi svoj sudoku:  \n')
+        sud=sud.split(',')
+        seznam=[]
+        for i in range(len(sud)):
+            for j in range(0,7,2):
+                if sud[i][j]!='0':
+                    seznam.append((i+1,j//2+1,int(sud[i][j])))
+
+    resitev=dpll.dpll(sudoku.sudoku4(seznam))[1]
+    for i in resitev.copy():
+        if resitev[i]==False:
+            del resitev[i]
+
     return risanje(resitev)
             
                     
