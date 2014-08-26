1a3b6e14aa60fcddd70ef329db1c2a4381e56282
podbregarb <barbara.podbregar@gmail.com>
2014-08-26 19:34:45 +0200

    Update primeri.py

--- Implementacija/primeri.py
+++ Implementacija/primeri.py
@@ -11,7 +11,12 @@ def primeri():
 
     # NNF oblika
     formula2 = formula1.nnf()
-    print('Formulo postavimo v nnf obliko: \n', formula2, '\n')
+    print('Formulo postavimo v nnf obliko: \n', formula2)
+
+    # CNF oblika
+    cnf1 = formula1.cnf()
+    print('Formulo postavimo v cnf obliko: \n', cnf1, '\n')
+    
 
     # Primer CNF formule:
     cnf0 = cnf.Cnf([
@@ -19,21 +24,20 @@ def primeri():
              cnf.Stavek([cnf.Lit("x"), cnf.Lit("y"), cnf.Til("z")]),
              cnf.Stavek([cnf.Lit("z")]),
            ])
-    print('Primer izpisa formule  v cnf obliki: \n', cnf0)
+    print('Primer izpisa formule  v cnf obliki: \n', cnf0, '\n')
 
     # CNF oblika
-    cnf1 = formula1.cnf()
-    print('Formulo postavimo v cnf obliko: \n', formula2, '\n')
-
     formula3 = bool.Or([bool.And([bool.Atom("a"), bool.Atom("b")]),
                    bool.And([bool.Atom("x"), bool.Atom("y")]),
                    bool.And([bool.Atom("u"), bool.Atom("v")])])
     cnf3 = formula3.cnf()
-    print('Formulo \n', formula3, '\n', 'postavimo v cnf obliko: \n', formula3, '\n')
+    print('Formulo \n', formula3, '\n postavimo v cnf obliko: \n', cnf3, '\n')
 
     formula4 = bool.Not(bool.And([bool.Not(bool.And([bool.Atom("a"), bool.Not(bool.Atom("b"))])),
                         bool.And([bool.Atom("x"), bool.Atom("y")]),
                         bool.And([bool.Atom("u"), bool.Atom("v")])]))
     cnf4 = formula4.cnf()
-    print('Formulo \n', formula4, '\n', 'postavimo v cnf obliko: \n', formula4, '\n')
+
+    print('Formulo \n', formula4, '\n postavimo v cnf obliko: \n', cnf4, '\n')
+
 
